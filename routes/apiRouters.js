6998c9ce72847880a1568d458a92ea123d66b6c6
caglarpanus <panuscaglar@gmail.com>
2018-03-07 19:02:54 -0500

    version 1 to write cleaner code

---
+++ routes/apiRouters.js
@@ -0,0 +1,2 @@
+var tableData = require("../data/tableData.js");
+var waitingListDate = require("../data/waitingListData.js");
--- server.js
+++ server.js
@@ -1,32 +1,10 @@
-const express = require('express');
-const path = require('path');
-const tables = require('./data.js');
-const tablesFull = require('./dataFull.js')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express();
+var app = express();
 
-const PORT = process.env.PORT || process.argv[2] || 8080;
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
-require('./routes/htmlRoutes.js')(app)
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-});
-
-app.get('/api', (req,res) => {
-    console.log(tables)
-    res.json(tables);
-})
-
-app.post('/api', (req, res) => {
-    if(tables.length < 5){
-        tables.push(req.body)
-    } else {
-        tablesFull.push(req.body)
-    }
-    console.log(req.body)
-})
+require("/routes/apiRoutes.js");
+require("/routes/htmlRoutes.js");
