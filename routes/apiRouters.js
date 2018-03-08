0c3f536e0a012d3d2ed777b0ac5e6328a641af59
caglarpanus <panuscaglar@gmail.com>
2018-03-07 19:55:58 -0500

    small fixes in html

--- routes/apiRouters.js
+++ routes/apiRouters.js
@@ -1,8 +1,30 @@
 var tableData = require("../data/tableData.js");
-var waitingListDate = require("../data/waitingListData.js");
+var waitingListData = require("../data/waitingListData.js");
 
+// app is "express". 
 module.exports = function(app){
     app.get("/api/tables", function(req,res){
         res.json(tableData);
+    });
+
+    app.get("/api/waitlist", function(req,res){
+        res.json(waitingListData);
+    });
+
+    app.post("/api/tables", function(req,res){
+        if(tableData.length < 5 ){
+            tableData.push(req.body);
+            res.json(tableData)
+        }
+        else{
+            waitingListData.push(req.body)
+            res.json(waitingListData);
+        }
+    });
+
+    app.post("/api/clear", function(req,res){
+        
+        tableData = [];
+        waitingListData = [];
     });
 }
